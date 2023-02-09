74190949bd2149ec20931c8e8b5a1d9b8492d5a9
watrgoat <oliveradams@iceeducate.org>
2023-02-09 15:57:23 -0500

    added data gathering

--- data.py
+++ data.py
@@ -1,4 +1,16 @@
 import yfinance as yf
 import pandas as pd
 
-df = yf.ticker('')
+# s&p 500 ticker symbol
+snp = yf.Ticker('^GSPC')
+
+df = snp.history(period='max', interval='1mo')
+df = df.reset_index()['Close']
+
+m_returns = []
+# Monthly Return = (Closing Price on Month / Closing Price Previous Month) - 1
+for i in range(1, len(df)-1):
+    m_returns.append((df[i]/df[i-1])-1)
+
+df = pd.DataFrame(m_returns, columns=['Returns'])
+df.to_csv('returns.csv')
